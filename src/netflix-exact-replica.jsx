30a3262e23e3827ca69aad3dcdbfa7859ae5592b
Dev Golakiya <devgolakiya3@gmail.com>
2026-06-03 13:25:20 -0400

    Add files via upload

--- src/netflix-exact-replica.jsx
+++ src/netflix-exact-replica.jsx
@@ -323,7 +323,7 @@ export default function NetflixPortfolio() {
             </a>
           </div>
           <a
-            href="https://drive.google.com/file/d/144I6Vnd5QfVWu780qQjfF-DdRnBI6by8/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1m7gth-m56MJU0HCtTjUk__YwXR9KKZ7s/view?usp=drive_link"
             className="inline-flex items-center gap-2 bg-red-600 text-white px-8 py-4 rounded-lg hover:bg-red-700 transition font-semibold text-lg"
           >
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
